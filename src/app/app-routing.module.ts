54d5aa8a07dc2c14d87a2a50072d283c053c80c9
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-04-19 14:19:07 +0300

    modified: main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
 
-const routes: Routes = [];
+import { RouterModule, Routes } from "@angular/router";
+import { SearchCompComponent } from "./search-comp/search-comp.component";
+import { RepositoriesComponent } from "./repositories/repositories.component";
+import { NotfoundComponent } from "./notfound/notfound.component";
+
+const routes: Routes = [
+  { path: "search", component: SearchCompComponent },
+  { path: "repos", component: RepositoriesComponent },
+  { path: "", redirectTo: "/search", pathMatch: "full" },
+  { path: "err", component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { RepositoriesComponent } from "./repositories/repositories.component";
+import { NotfoundComponent } from "./notfound/notfound.component";
+import { SearchCompComponent } from "./search-comp/search-comp.component";
+import { NavComponent } from "./nav/nav.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { NgProgressModule } from "@ngx-progressbar/core";
+import { NgProgressHttpClientModule } from "@ngx-progressbar/http-client";
+import { AgoPipe } from './pipes/ago.pipe';
+import { HiglightDirective } from './directives/higlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoriesComponent,
+    NotfoundComponent,
+    SearchCompComponent,
+    NavComponent,
+    AgoPipe,
+    HiglightDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule,
   ],
+  
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
